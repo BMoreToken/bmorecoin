bcef862fc9ed69990f1f6d7060452a0ccc6881f8
Patrick McGuire <insidenothing@gmail.com>
2022-03-14 18:58:51 -0400

    Update address_book.h

--- src/Wallet/api/address_book.h
+++ src/Wallet/api/address_book.h
@@ -28,8 +28,8 @@
 //
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
-#include "wallet/api/wallet2_api.h"
-#include "wallet/wallet2.h"
+#include "Wallet/api/wallet2_api.h"
+#include "Wallet/wallet2.h"
 
 namespace Monero {
 
