6116b371b3d95e07356d5e825d923485e86a0bbe
Patrick McGuire <insidenothing@gmail.com>
2022-03-14 18:49:18 -0400

    yea

--- src/multisig/multisig.h
+++ src/multisig/multisig.h
@@ -31,7 +31,7 @@
 #include <vector>
 #include <unordered_map>
 #include "crypto/crypto.h"
-#include "cryptonote_basic/cryptonote_format_utils.h"
+#include "CryptoNoteCore/cryptonote_format_utils.h"
 #include "ringct/rctTypes.h"
 
 namespace cryptonote
